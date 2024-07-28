b475172c302e1f856bb0d4c80d5378a70210e51a
Muhammed Samson <111194652+Muhammed-shamsadin@users.noreply.github.com>
2024-07-29 01:02:08 +0300

    new Upload

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,12 @@ import { AuthProvider } from './auth/authContext';
 import TeamLeadLogin from './pages/TeamLead/teamLeadLogin';
 import ManagerLogin from './pages/Manager/ManagerLogin';
 import HRLogin from './pages/HR/HRLogin';
-
+import TeamLeadPage from './pages/TeamLead/TeamLeadPage';
+import ManagerPage from './pages/Manager/ManagerPage';
+import PostJob from './pages/TeamLead/PostJob';
+import PostedStatus from './pages/TeamLead/PostedStatus';
+import PostedJobs from './pages/Manager/PostedJobs';
+import StatusHistory from './pages/Manager/StatusHistory';
 
 const App = () => {
   return (
@@ -15,9 +20,19 @@ const App = () => {
       <Router>
         <Routes>
           <Route path='/teamlead-login' element={ <TeamLeadLogin /> } />
+          <Route path='/post-job' element={ <PostJob /> } />
+          <Route path='/posted-status' element={ <PostedStatus /> } />
+
           <Route path='/manager-login' element={ <ManagerLogin /> } />
+          <Route path='/see-posted-job' element={ <PostedJobs /> } />
+          <Route path='/see-history' element={ <StatusHistory /> } />
+          
+
           <Route path='/hr-login' element={ <HRLogin /> } />
+          
           <Route path='/dashboard' element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path='/teamlead-page' element={<ProtectedRoute><TeamLeadPage /></ProtectedRoute>} />
+          <Route path='/manager-page' element={<ProtectedRoute><ManagerPage /></ProtectedRoute>} />
           <Route path='/' element={<Homepage />} />
         </Routes>
       </Router>
