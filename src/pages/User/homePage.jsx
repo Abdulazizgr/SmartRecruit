b3ce953205d08efb23eec1498aa12212eef33922
muhammed-shamsadin <muhammedsamsoon@gmail.com>
2024-08-02 22:55:38 +0300

    Benefit section commented out

--- src/pages/User/homePage.jsx
+++ src/pages/User/homePage.jsx
@@ -1,5 +1,24 @@
-const Homepage = () => {
-    return <h1>Homepage</h1>;
+import React from 'react';
+import HeroSection from './HeroSection';
+import AboutSection from './AboutSection';
+import ApplicationProcessSection from './ApplicationProcessSection';
+import JobListingsSection from './JobListingsSection';
+// import BenefitsSection from './BenefitsSection';
+import Footer from './Footer';
+import Navbar from './Navbar';
+
+const HomePage = () => {
+  return (
+    <>
+      <Navbar/>
+      <HeroSection />
+      <AboutSection />
+      <ApplicationProcessSection />
+      <JobListingsSection />
+      {/* <BenefitsSection /> */}
+      <Footer />
+    </>
+  );
 };
 
-export default Homepage;
+export default HomePage;
