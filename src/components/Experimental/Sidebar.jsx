6bb3f8ee71a84f1b87d35730b5d0d1766e513955
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-08-07 11:59:41 +0300

    Modified the Sidebar to be more Responsive using toggle

--- src/components/Experimental/Sidebar.jsx
+++ src/components/Experimental/Sidebar.jsx
@@ -1,89 +1,103 @@
-import React from 'react';
-
-
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import SettingsAccessibilityIcon from '@mui/icons-material/SettingsAccessibility';
-import DepartmentIcon from '@mui/icons-material/Business';
-import EmployeeIcon from '@mui/icons-material/People';
-import ApplicantsIcon from '@mui/icons-material/PersonAdd';
-import JobPostingIcon from '@mui/icons-material/Work';
-import NotificationIcon from '@mui/icons-material/Notifications';
-import SettingsIcon from '@mui/icons-material/Settings';
-import ProfileIcon from '@mui/icons-material/AccountCircle';
-import LogoutIcon from '@mui/icons-material/Logout';
-import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
-import { EmojiEvents } from '@mui/icons-material';
+import React, { useState } from 'react';
+import {
+  Dashboard as DashboardIcon,
+  SettingsAccessibility as SettingsAccessibilityIcon,
+  Business as DepartmentIcon,
+  People as EmployeeIcon,
+  PersonAdd as ApplicantsIcon,
+  Work as JobPostingIcon,
+  Notifications as NotificationIcon,
+  Settings as SettingsIcon,
+  AccountCircle as ProfileIcon,
+  Logout as LogoutIcon,
+  CalendarToday as CalendarTodayIcon,
+  EmojiEvents,
+  KeyboardDoubleArrowRight as MenuIcon,
+} from '@mui/icons-material';
 
 const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className=" flex-1 min-h-screen bg-[white] border-r-[0.5px] border-r-[rgba(230,227,227)] border-solid">
+    <div className={` min-h-screen bg-white border-r border-gray-300 transition-all duration-300 ${isOpen ? 'flex-1 w-64' : 'w-25'}`}>
       {/* TOP */}
-      <div className=" h-[50px] flex items-center justify-center">
-        <span className="text-[19px] font-[bold] text-[#6439ff]">IE-Network</span>
+      <div className="h-12 flex items-center justify-between p-2">
+      <img
+              src="../../../public/assets/IElogo.png"
+              alt='profile-picture'
+              className='w-[30px] h-[30px] bg-transparent rounded-sm'
+            />
+        {isOpen && <span className="text-2xl font-bold text-accent">HR-Admin</span>}
+        <MenuIcon 
+          onClick={toggleSidebar} 
+          className={`cursor-pointer text-accent transition-transform duration-300 ${isOpen ? '' : 'rotate-180'}`} 
+        />
       </div>
-      <hr className='h-0 border-[0.5px] border-solid border-[rgba(230,227,227)]'/>
+      <hr className='border-gray-300' />
       {/* CENTER */}
-      <div className="pl-5">
-        <ul className='m-0 p-0'>
-          <p className='text-[10px] font-[bold] text-[#999] mt-[15px] mb-2.5'>MAIN</p>
-          <li className='flex items-center cursor-pointer p-[5px] hover:bg-[#ece8ff]'>
-            <DashboardIcon className='text-lg text-[#7451f8]'/>
-            <span className='text-[13px] font-semibold text-[#888] ml-2.5'>Dashboard</span>
+      <div className={` overflow-y-auto h-[calc(100vh-50px)] ${isOpen ? 'pl-4' : 'pl-2'}`}>
+        <ul className='m-0 p-0 '>
+          <p className={`text-xs font-bold text-gray-500 ${isOpen ? 'mt-4 mb-2.5' : 'mt-4 mb-1'}`}>Main</p>
+          <li className='flex items-center cursor-pointer p-2 hover:bg-purple-100 '>
+            <DashboardIcon className='text-lg text-accent' />
+            {isOpen && <span className='text-sm font-semibold text-primary ml-2.5'>Dashboard</span>}
           </li>
-          <li className='flex items-center cursor-pointer p-[5px] hover:bg-[#ece8ff]'>
-            <SettingsAccessibilityIcon className='text-lg text-[#7451f8]' />
-            <span className='text-[13px] font-semibold text-[#888] ml-2.5'>Attendance</span>
+          <li className='flex items-center cursor-pointer p-2 hover:bg-purple-100'>
+            <SettingsAccessibilityIcon className='text-lg text-accent' />
+            {isOpen &&  <span className='text-sm font-semibold text-primary ml-2.5'>Attendance</span>}
           </li>
-          <li className='flex items-center cursor-pointer p-[5px] hover:bg-[#ece8ff]'>
-            <CalendarTodayIcon className='text-lg text-[#7451f8]'/>
-            <span className='text-[13px] font-semibold text-[#888] ml-2.5'>Calendar</span>
+          <li className='flex items-center cursor-pointer p-2 hover:bg-purple-100'>
+            <CalendarTodayIcon className='text-lg text-accent' />
+            {isOpen &&   <span className='text-sm font-semibold text-primary ml-2.5'>Calendar</span>}
           </li>
-
-          <p className='text-[10px] font-[bold] text-[#999] mt-[15px] mb-2.5'>Management</p>
-          <li className='flex items-center cursor-pointer p-[5px] hover:bg-[#ece8ff]'>
-            <DepartmentIcon className='text-lg text-[#7451f8]' />
-            <span className='text-[13px] font-semibold text-[#888] ml-2.5'>Department</span>
+          <p className={`text-xs font-bold text-gray-500 ${isOpen ? 'mt-4 mb-2.5' : 'mt-4 mb-1'} `} >Control</p>
+          <li className='flex items-center cursor-pointer p-2 hover:bg-purple-100'>
+            <DepartmentIcon className='text-lg text-accent' />
+            {isOpen &&    <span className='text-sm font-semibold text-primary ml-2.5'>Department</span>}
           </li>
-          <li className='flex items-center cursor-pointer p-[5px] hover:bg-[#ece8ff]'>
-            <EmployeeIcon className='text-lg text-[#7451f8]'/>
-            <span className='text-[13px] font-semibold text-[#888] ml-2.5'>Employee</span>
+          <li className='flex items-center cursor-pointer p-2 hover:bg-purple-100'>
+            <EmployeeIcon className='text-lg text-accent' />
+            {isOpen &&   <span className='text-sm font-semibold text-primary ml-2.5'>Employee</span> }
           </li>
-          <li className='flex items-center cursor-pointer p-[5px] hover:bg-[#ece8ff]'>
-            <ApplicantsIcon className='text-lg text-[#7451f8]'/>
-            <span className='text-[13px] font-semibold text-[#888] ml-2.5'>Applicants</span>
+          <li className='flex items-center cursor-pointer p-2 hover:bg-purple-100'>
+            <ApplicantsIcon className='text-lg text-accent' />
+            {isOpen &&    <span className='text-sm font-semibold text-primary ml-2.5'>Applicants</span>}
           </li>
-          <li className='flex items-center cursor-pointer p-[5px] hover:bg-[#ece8ff]'>
-            <JobPostingIcon className='text-lg text-[#7451f8]'/>
-            <span className='text-[13px] font-semibold text-[#888] ml-2.5'>Job Posting</span>
+          <li className='flex items-center cursor-pointer p-2 hover:bg-purple-100'>
+            <JobPostingIcon className='text-lg text-accent' />
+            {isOpen &&  <span className='text-sm font-semibold text-primary ml-2.5'>Job Posting</span>}
           </li>
-          <li className='flex items-center cursor-pointer p-[5px] hover:bg-[#ece8ff]'>
-            <EmojiEvents className='text-lg text-[#7451f8]'/>
-            <span className='text-[13px] font-semibold text-[#888] ml-2.5'>Award</span>
+          <li className='flex items-center cursor-pointer p-2 hover:bg-purple-100'>
+            <EmojiEvents className='text-lg text-accent' />
+            {isOpen &&  <span className='text-sm font-semibold text-primary ml-2.5'>Award</span>}
           </li>
-          <li className='flex items-center cursor-pointer p-[5px] hover:bg-[#ece8ff]'>
-            <NotificationIcon className='text-lg text-[#7451f8]'/>
-            <span className='text-[13px] font-semibold text-[#888] ml-2.5'>Notification</span>
+          <li className='flex items-center cursor-pointer p-2 hover:bg-purple-100'>
+            <NotificationIcon className='text-lg text-accent' />
+            {isOpen &&  <span className='text-sm font-semibold text-primary ml-2.5'>Notification</span>}
           </li>
-
-          <p className='text-[10px] font-[bold] text-[#999] mt-[15px] mb-2.5'>Account</p>
-          <li className='flex items-center cursor-pointer p-[5px] hover:bg-[#ece8ff]'>
-            <SettingsIcon className='text-lg text-[#7451f8]'/>
-            <span className='text-[13px] font-semibold text-[#888] ml-2.5'>Settings</span>
+          <p className={`text-xs font-bold text-gray-500 ${isOpen ? 'mt-4 mb-2.5' : 'mt-4 mb-1'}`}>Account</p>
+          <li className='flex items-center cursor-pointer p-2 hover:bg-purple-100'>
+            <SettingsIcon className='text-lg text-accent' />
+            {isOpen &&    <span className='text-sm font-semibold text-primary ml-2.5'>Settings</span>}
           </li>
-          <li className='flex items-center cursor-pointer p-[5px] hover:bg-[#ece8ff]'>
-            <ProfileIcon className='text-lg text-[#7451f8]'/>
-            <span className='text-[13px] font-semibold text-[#888] ml-2.5'>Profile</span>
+          <li className='flex items-center cursor-pointer p-2 hover:bg-purple-100'>
+            <ProfileIcon className='text-lg text-accent' />
+            {isOpen &&     <span className='text-sm font-semibold text-primary ml-2.5'>Profile</span>}
           </li>
-          <li className='flex items-center cursor-pointer p-[5px] hover:bg-[#ece8ff]'>
-            <LogoutIcon className='text-lg text-[#7451f8]'/>
-            <span className='text-[13px] font-semibold text-[#888] ml-2.5'>Logout</span>
+          <li className='flex items-center cursor-pointer p-2 hover:bg-purple-100'>
+            <LogoutIcon className='text-lg text-accent' />
+            {isOpen &&  <span className='text-sm font-semibold text-primary ml-2.5'>Logout</span>}
           </li>
         </ul>
       </div>
-      {/* BOTTOm */}
-  
     </div>
   );
 };
 
 export default Sidebar;
+
+
