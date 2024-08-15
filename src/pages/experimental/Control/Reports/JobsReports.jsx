d9bd11d5c367f44a2ef415f255a2fd9bf6cc9b19
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-08-15 10:56:51 +0300

    Modified to support jobsreportrable

--- src/pages/experimental/Control/Reports/JobsReports.jsx
+++ src/pages/experimental/Control/Reports/JobsReports.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Sidebar from '../../../../components/Experimental/Sidebar'
 import Navbar from '../../../../components/Experimental/Navbar'
+import JobReportTable from '../../../../components/JobsReportTable'
 
 const JobsReports = () => {
   return (
@@ -8,10 +9,10 @@ const JobsReports = () => {
     <Sidebar />
     <div className="flex-[6]">
       <Navbar/> {/* Pass props */}
-      <div className="p-4 flex flex-col lg:flex-row">
-        <div className="flex-1 lg:mr-4 h-full">
-         <div>Jobs Reports</div> {/* Pass searchTerm */}
-        </div>
+      <div className="p-4 flex bg-gray-300 flex-col lg:flex-row">
+          <div className="ml-5 bg-white rounded-md flex-1 lg:mr-5 h-full m-1 shadow-gray-500 shadow-md w-min">
+            <JobReportTable />
+          </div>
       </div>
     </div>
   </div>
